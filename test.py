532fe99af1a9b8ec9a7df21c57cedd6ea917f4f7
Jose Bouza <josejbouza@gmail.com>
2019-05-04 19:10:29 -0400

    test file

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ import torch
 import time
 import cuda_batch_inverse
 
-A = torch.rand(10000,64,64).cuda()
+A = torch.rand(50000,64,64).cuda()
 
-#start2 = time.time()
-#print(torch.inverse(A))
-#end2 = time.time()
+
+torch.inverse(A)
+cuda_batch_inverse.forward(A)
+start2 = time.time()
+print(torch.inverse(A))
+end2 = time.time()
 start1 = time.time()
 print(cuda_batch_inverse.forward(A))
 end1 = time.time()
 
-
+print("cuBLAS:")
 print(end1-start1)
-#print(end2-start2)
+print("MAGMA:")
+print(end2-start2)
