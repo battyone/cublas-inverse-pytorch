7f69eaff23029628339c8bb3470c1093ea6b3920
Jose Bouza <josejbouza@gmail.com>
2019-05-05 16:22:56 -0400

    Added backward

--- cuda_inverse.py
+++ cuda_inverse.py
@@ -1,15 +1,14 @@
 import torch
 import cuda_batch_inverse
 
-class cuda_inverse_op(torch.autograd.Function):
+class cuda_inverse(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input):
         output = cuda_batch_inverse.forward(input)
         ctx.save_for_backward(output);
-
         return output;
-    
-    def backward(ctx, grad_output):
 
-def cuda_inverse(input):
-    return cuda_inverse_op.apply(input)
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = cuda_batch_inverse.backward(ctx.saved_variables[0], grad_output)
+        return output
